2fa8784910a12cfc2f2d7e7c2a4ab1880fefa62c
zengfufu <ilywch@163com>
2015-04-25 21:22:25 +0800

    修改人：zeng
    1。加入spring  jar包
    2。加入commons-logging-1.1.1.jar

---
+++ src/com/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		System.out.println("sdfsdf");
+		// TODO Auto-generated method stub
+		return new ModelAndView("/index");
+	}
+
+}
